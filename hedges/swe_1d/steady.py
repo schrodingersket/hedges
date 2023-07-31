32139f53142dd3635e50b45702fcf91b4096b6fb
schrodingersket <schrodingersket@gmail.com>
2023-07-31 14:46:44 -0600

    Updated analytic subcritical SWE steady-state solution to account for variable bathymetry.

--- hedges/swe_1d/steady.py
+++ hedges/swe_1d/steady.py
@@ -1,24 +1,26 @@
 import numpy as np
+import scipy.optimize
 
-from scipy import optimize
 
-
-def subcritical_height(g, bb, q0, hL):
+def subcritical_height(gravity, b, q_l, h_r, b_r=0):
     """
     Computes the steady state height at set of points in the computational domain of
     subcritical Shallow Water flow over a bump.
 
     See Section 3.1.3 of https://doi.org/10.1002/fld.3741 for more information.
 
-    :param g: Gravitational constant
-    :param bb: Bathymetry values at computational domain points
-    :param q0: Prescribed steady state flow value (steady-state solutions require constant flow
-               across domain)
-    :param hL: Prescribed height at x=L (right boundary)
+    :param gravity: Gravitational constant
+    :param b: Array of bathymetry values at computational domain points
+    :param q_l: Prescribed steady state flow at x = x_l (left boundary)
+    :param h_r: Prescribed height at x = x_r (right boundary)
+    :param b_r: Bathymetry height at x = x_r (right boundary)
     :return:
     """
-    return np.array(list(map(lambda z: optimize.newton(
-        lambda h: np.power(h, 3) + (z - np.square(q0/hL)/(2*g) - hL) * np.square(h) + np.square(q0)/(2*g),
-        x0=hL,
+    c = (b - (b_r + h_r) - np.square(q_l / h_r) / (2 * gravity))
+    k = np.square(q_l) / (2 * gravity)
+
+    return np.array([scipy.optimize.newton(
+        lambda h: np.power(h, 3) + c[j] * np.square(h) + k,
+        x0=h_r,
         maxiter=500
-    ), bb)))
+    ) for j, _ in enumerate(b)])
